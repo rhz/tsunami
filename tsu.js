3fabd6ffe05358ce64919b2b93dc75fac855fd59
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2018-12-05 20:27:07 -0300

    added collision detection with other people and tidied up a bit the code

--- tsu.js
+++ tsu.js
@@ -4,10 +4,13 @@ var active = false,
     people = [],
     delay = 1000,
     dlatmax = 0.0001, // max delta latitude
-    dlngmax = 0.0001  // max delta longitude
+    dlngmax = 0.0001, // max delta longitude
+    centre = [-33.0421, -71.6123],
+    zoomLevel = 16,
+    personRadius = 1
 
 // load map and tiles
-var valpo = L.map('map').setView([-33.0421, -71.6123], 15)
+var valpo = L.map('map').setView(centre, zoomLevel)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     maxZoom: 19
@@ -15,10 +18,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // move a person on the map
 function movePerson(p) {
-  var {lat, lng} = p.getLatLng(),
-      dlat = Math.random()*2*dlatmax-dlatmax,
-      dlng = Math.random()*2*dlngmax-dlngmax
-  p.setLatLng([lat+dlat, lng+dlng])
+  var ppos = p.getLatLng(),
+      dlat = Math.random()*2*dlatmax-dlatmax, // delta lat
+      dlng = Math.random()*2*dlngmax-dlngmax, // delta lng
+      npos = [ppos.lat+dlat, ppos.lng+dlng], // new position
+      overlap = false
+  for (var q of people) {
+    if (q !== p) {
+      var qpos = q.getLatLng(),
+          dist = valpo.distance(npos, qpos)
+      if (dist < 2*personRadius) {
+        overlap = true
+        break
+      }
+    }
+  }
+  if (!overlap)
+    p.setLatLng(npos)
 }
 function movePersonAfterDelay(p) {
   var id = window.setTimeout(movePersonRepeat(p), delay)
@@ -37,7 +53,7 @@ function addPerson(latlng) {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 1
+    radius: personRadius
   }).addTo(valpo)
   // people moves on the map
   active = true // play()
@@ -67,76 +83,81 @@ function play() {
     movePersonAfterDelay(p)
 }
 
-// parse pbf file
+// logging element
 var logEle = document.getElementById("log");
 function log(msg) {
   logEle.textContent += msg + '\n';
 }
+
 var nodeCount = 0,
     wayCount = 0,
     relCount = 0,
     polygonCount = 0,
-    buildingCount = 0,
     start = Date.now(),
-    nodes = {}
-// var first = true
+    nodes = {},
+    buildings = []
+
+// helper function
 function last(xs) {
   return xs[xs.length-1]
 }
+
+// polygons
 function isPolygon(way) {
   return way.nodeRefs[0] === last(way.nodeRefs)
 }
-function drawPolygon(way) {
+
+function drawPolygon(vertices) {
   var latlons = []
-  for (nodeId of way.nodeRefs) {
+  for (nodeId of vertices) {
     latlons.push([nodes[nodeId].lat, nodes[nodeId].lon])
   }
-  // valpo.panTo(latlons[0])
+  valpo.panTo(latlons[0])
   return L.polygon(latlons).addTo(valpo);
 }
-var a = pbfParser.parse({
+
+function afterParse() {
+  // log('way: ' + JSON.stringify(buildings[0]))
+  // drawPolygon(buildings[0].vertices)
+}
+
+// parse pbf file
+pbfParser.parse({
   filePath: 'valpo.osm.pbf',
   endDocument: function() {
+    // print some stats of the map
     log(nodeCount + ' nodes')
-    log(wayCount + ' ways of which ' + buildingCount +
+    log(wayCount + ' ways of which ' + buildings.length +
         ' are buildings and ' + polygonCount + ' are polygons')
     log(relCount + ' relations')
     log('finished reading ' + this.filePath + ' in ' +
         (Date.now() - start)/1000 + ' seconds')
-  },
-  bounds: function(bounds) {
-    log('bounds: ' + JSON.stringify(bounds))
+    // continue with the execution of model
+    afterParse()
   },
   node: function(node) {
     nodeCount += 1
     nodes[node.id] = node
-    // log('node: ' + JSON.stringify(node))
   },
   way: function(way) {
     wayCount += 1
     if (isPolygon(way))
       polygonCount += 1
     if ('building' in way.tags) {
-      buildingCount += 1
-      // TODO: there's one way which is a building and not a polygon
+      buildings.push({vertices: way.nodeRefs})
+      // NOTE: there's one way which is a building and not a polygon
       // maybe a building that was cut in half
       // when generating the pbf file?
-      // if (!isPolygon(way))
-      //   log('way: ' + JSON.stringify(way))
-      // if (first) {
-      //   drawPolygon(way)
-      //   log('way: ' + JSON.stringify(way))
-      //   first = false
-      // }
+      if (!isPolygon(way))
+        log('building is not a polygon: ' + JSON.stringify(way))
     }
-    // log('way: ' + JSON.stringify(way))
   },
   relation: function(relation) {
     relCount += 1
-    // log('relation: ' + JSON.stringify(relation))
   },
   error: function(msg) {
     log('error: ' + msg)
     throw msg
   }
 })
+
